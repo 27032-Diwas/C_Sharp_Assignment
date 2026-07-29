20273e545a81323b22b42097676bb6f4fb976900
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-07-29 13:57:34 +0530

    feat: IController

--- src/InventoryManager/Controller/IController.cs
+++ src/InventoryManager/Controller/IController.cs
@@ -5,4 +5,28 @@
 /// </summary>
 public interface IController
 {
+    /// <summary>
+    /// Calls get product details and create object with the detials.
+    /// </summary>
+    public void AddContact();
+
+    /// <summary>
+    /// Get product id of product to remove and call service.
+    /// </summary>
+    public void RemoveContact();
+
+    /// <summary>
+    /// Getnew details and create an object and pass it to service.
+    /// </summary>
+    public void UpdateContact();
+
+    /// <summary>
+    /// Get product list and call display product.
+    /// </summary>
+    public void ViewAllProducts();
+
+    /// <summary>
+    /// Get search word from user and calls service.
+    /// </summary>
+    public void SearchProducts();
 }
