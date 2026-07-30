9eedcae8481481552ffed5babe4241ae62042fae
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-07-30 16:44:04 +0530

    fix:folder structure

--- src/InventoryManager/Constants/ProductConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryManager.Constants;
-
-/// <summary>
-/// Contains constant related to products.
-/// </summary>
-protected static class ProductConstants
-{
-    public const string ClothingPrefix = "CLTH";
-}
