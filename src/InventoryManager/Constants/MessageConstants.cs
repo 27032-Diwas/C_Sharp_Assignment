3909d19418669bc412736929c221e17115130485
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-05 11:57:39 +0530

    Inventory Manager | Add product (#4)
    * feat:add contact serice
    
    * feat: add product
    
    * docs: constant messages
    
    * fix: console user friendly display
    
    * fix:warning
    
    * fix:product id increment
    
    * fix:typos
    
    * fix: Peer review comments.
    
    * fix:documentation in view
    
    * fix: interface and documentation
    
    * fix: documentation
    
    * fix: warnings
    
    * fix: seperation of constant messages
    
    * fix: constant message in view

--- src/InventoryManager/Constants/MessageConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InventoryManager.Constants;
-
-/// <summary>
-/// Contains all the repeated strings in the project.
-/// </summary>
-public static class MessageConstants
-{
-}
---
+++ src/InventoryManager/Controller/InventoryMenuController.cs
@@ -0,0 +1,57 @@
+﻿using InventoryManager.Constants;
+using InventoryManager.EnumConstants;
+using InventoryManager.Repository;
+using InventoryManager.Service;
+using InventoryManager.View;
+
+namespace InventoryManager.Controller;
+
+/// <summary>
+/// Contains menu and gets user choice.
+/// </summary>
+public class InventoryMenuController
+{
+    private readonly InventoryController _inventoryController;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InventoryMenuController"/> class.
+    /// </summary>
+    /// <param name="inventoryController"> Instance of inventory controller. </param>
+    public InventoryMenuController(InventoryController inventoryController)
+    {
+        this._inventoryController = inventoryController;
+    }
+
+    /// <summary>
+    /// Displays the main menu and proceed to selected option.
+    /// </summary>
+    public void GetMenuOption()
+    {
+        while (true)
+        {
+            MainMenuEnum choice = DisplayEnum.GetMenuChoice<MainMenuEnum>(HeaderMessages.MainMenu);
+            InventoryView.ClearConsole();
+            switch (choice)
+            {
+                case MainMenuEnum.Exit:
+                    InventoryView.DisplayMessage(SuccessMessages.ProcessEnded);
+                    return;
+                case MainMenuEnum.AddProduct:
+                    InventoryView.DisplayMessage(HeaderMessages.AddProduct);
+                    this._inventoryController.AddProduct();
+                    break;
+                case MainMenuEnum.ViewProducts:
+                    break;
+                case MainMenuEnum.SearchProduct:
+                    break;
+                case MainMenuEnum.UpdateProduct:
+                    break;
+                case MainMenuEnum.RemoveProduct:
+                    break;
+                default:
+                    InventoryView.DisplayMessage(ErrorMessages.InvalidOption);
+                    break;
+            }
+        }
+    }
+}
