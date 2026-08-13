2d3ef87207891d08a890efa702a49bcca8264710
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 15:24:01 +0530

    build: userprompts

--- src/ExpenseTracker/Constants/UserPrompts.cs
+++ src/ExpenseTracker/Constants/UserPrompts.cs
@@ -5,4 +5,48 @@
 /// </summary>
 public class UserPrompts
 {
+    /// <summary>
+    /// Represents the prompt requesting the user to select one of the available options.
+    /// </summary>
+    public const string SelectOption = "Select one of the above options:";
+
+    /// <summary>
+    /// Represents the message prompted to get user input to exit current process.
+    /// </summary>
+    public const string ExitProcess = "or Exit to quit process:";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to select one of the available options.
+    /// </summary>
+    public const string GetYesOrNo = "Do you want to choose above product [Y/N]:";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to press any key to continue.
+    /// </summary>
+    public const string GetAnyKey = "PRESS ANY KEY TO CONTINUE!!";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to enter transaction amount.
+    /// </summary>
+    public const string GetAmount = "Enter transaction amount:";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to enter transaction date.
+    /// </summary>
+    public const string GetDate = "Enter transaction date:";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to enter transaction description.
+    /// </summary>
+    public const string GetDescription = "Enter transaction description:";
+
+    /// <summary>
+    /// Represents the message prompted to get word to search.
+    /// </summary>
+    public const string GetSearchWord = "Enter word to search product:";
+
+    /// <summary>
+    /// Represents the prompt requesting the user to enter the serial number.
+    /// </summary>
+    public const string SelectSerialNumber = "Enter a serial number:";
 }
