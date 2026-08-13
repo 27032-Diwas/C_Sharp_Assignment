c04c2dbe5b2e12b2ee3a444c8f59e1f7036f44da
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 10:54:45 +0530

    build: Enums

--- src/ExpenseTracker/EnumConstants/MainMenu.cs
+++ src/ExpenseTracker/EnumConstants/MainMenu.cs
@@ -5,4 +5,28 @@
 /// </summary>
 public enum MainMenu
 {
+    /// <summary>
+    /// Represents the option to exit the application.
+    /// </summary>
+    Exit,
+
+    /// <summary>
+    /// Represents the option to access add transaction method.
+    /// </summary>
+    AddTransaction,
+
+    /// <summary>
+    /// Represents the option to access view transaction method.
+    /// </summary>
+    ViewTransactions,
+
+    /// <summary>
+    /// Represents the option to access delete transaction method.
+    /// </summary>
+    DeleteTransaction,
+
+    /// <summary>
+    /// Represents the option to access delete transaction method.
+    /// </summary>
+    EditTransaction,
 }
--- src/ExpenseTracker/EnumConstants/TransactionTypes.cs
+++ src/ExpenseTracker/EnumConstants/TransactionTypes.cs
@@ -5,4 +5,18 @@
 /// </summary>
 public enum TransactionTypes
 {
+    /// <summary>
+    /// Represents the option to exit the process.
+    /// </summary>
+    Back,
+
+    /// <summary>
+    /// Represents the income transaction.
+    /// </summary>
+    Income,
+
+    /// <summary>
+    /// Represents the expense transaction.
+    /// </summary>
+    Expense,
 }
