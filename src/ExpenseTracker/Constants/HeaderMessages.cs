a294786734fe8458670245d196a9a2d8e775fa01
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 15:13:00 +0530

    build: header messages

--- src/ExpenseTracker/Constants/HeaderMessages.cs
+++ src/ExpenseTracker/Constants/HeaderMessages.cs
@@ -5,4 +5,48 @@
 /// </summary>
 public class HeaderMessages
 {
+    /// <summary>
+    /// Represents the header message for main menu.
+    /// </summary>
+    public const string MainMenu = "MAIN MENU";
+
+    /// <summary>
+    /// Represents the header message to exit application.
+    /// </summary>
+    public const string Exit = "EXIT";
+
+    /// <summary>
+    /// Represents the header message for add transaction.
+    /// </summary>
+    public const string AddTransaction = "ADD TRANSACTION";
+
+    /// <summary>
+    /// Represents the header message for view transaction.
+    /// </summary>
+    public const string ViewTransaction = "VIEW TRANSACTION";
+
+    /// <summary>
+    /// Represents the header message for search transaction.
+    /// </summary>
+    public const string SearchTransaction = "SEARCH TRANSACTION";
+
+    /// <summary>
+    /// Represents the header message for edit transaction.
+    /// </summary>
+    public const string EditTransaction = "EDIT TRANSACTION";
+
+    /// <summary>
+    /// Represents the header message for delete transaction.
+    /// </summary>
+    public const string DeleteTransaction = "DELETE TRANSACTION";
+
+    /// <summary>
+    /// Represents the header message for transaction types.
+    /// </summary>
+    public const string TransactionTypes = "TRANSACTION TYPES";
+
+    /// <summary>
+    /// Represents the header message for categories.
+    /// </summary>
+    public const string Categories = "CATEGORIES";
 }
