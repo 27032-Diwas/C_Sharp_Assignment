b5f5fa549d059597142aa68afda540e5fe1f68aa
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 10:55:27 +0530

    fix: EnumConstants to enums

--- src/ExpenseTracker/Enums/TransactionTypes.cs
+++ src/ExpenseTracker/Enums/TransactionTypes.cs
@@ -6,6 +6,11 @@
 public enum TransactionTypes
 {
     /// <summary>
+    /// Represents the option to exit the process.
+    /// </summary>
+    Back,
+
+    /// <summary>
     /// Represents the income transaction.
     /// </summary>
     Income = 1,
