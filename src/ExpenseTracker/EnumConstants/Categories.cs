bec62cec90ebb0faab0efb3e1e6b8706a44462e5
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 10:54:45 +0530

    build: Enums

--- src/ExpenseTracker/EnumConstants/Categories.cs
+++ src/ExpenseTracker/EnumConstants/Categories.cs
@@ -5,4 +5,63 @@
 /// </summary>
 public enum Categories
 {
+    /// <summary>
+    /// Represents the option to exit the process.
+    /// </summary>
+    Back,
+
+    /// <summary>
+    /// Represents the food category.
+    /// </summary>
+    Food,
+
+    /// <summary>
+    /// Represents the transport category.
+    /// </summary>
+    Transport,
+
+    /// <summary>
+    /// Represents the shopping category.
+    /// </summary>
+    Shopping,
+
+    /// <summary>
+    /// Represents the bills category.
+    /// </summary>
+    Bills,
+
+    /// <summary>
+    /// Represents the entertainment category.
+    /// </summary>
+    Entertainment,
+
+    /// <summary>
+    /// Represents the health category.
+    /// </summary>
+    Health,
+
+    /// <summary>
+    /// Represents the education category.
+    /// </summary>
+    Education,
+
+    /// <summary>
+    /// Represents the salary category.
+    /// </summary>
+    Salary,
+
+    /// <summary>
+    /// Represents the freelance category.
+    /// </summary>
+    Freelance,
+
+    /// <summary>
+    /// Represents the investment category.
+    /// </summary>
+    Investment,
+
+    /// <summary>
+    /// Represents the others category.
+    /// </summary>
+    Others,
 }
