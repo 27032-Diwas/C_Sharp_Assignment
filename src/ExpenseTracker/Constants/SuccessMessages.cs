9fb95a872f3670e262d7d281c9cb5e278db36276
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-13 15:19:27 +0530

    build: success messages

--- src/ExpenseTracker/Constants/SuccessMessages.cs
+++ src/ExpenseTracker/Constants/SuccessMessages.cs
@@ -5,4 +5,23 @@
 /// </summary>
 public class SuccessMessages
 {
+    /// <summary>
+    /// Represents the string message displayed when transaction added successfully.
+    /// </summary>
+    public const string SuccessfulAdditionOfTransaction = "TRANSACTION ADDED SUCCESSFULLY!!";
+
+    /// <summary>
+    /// Represents the string message displayed when transaction deleted successfully.
+    /// </summary>
+    public const string SuccessfulRemovalOfTransaction = "TRANSACTION DELETED SUCCESSFULLY!!";
+
+    /// <summary>
+    /// Represents the string message displayed when transaction updated successfully.
+    /// </summary>
+    public const string SuccessfulUpdatedTheTransaction = "TRANSACTION UPDATED SUCCESSFULLY!!";
+
+    /// <summary>
+    /// Represents the message displayed when the application process ends.
+    /// </summary>
+    public const string ProcessEnded = "THANK YOU FOR USING THIS APPLICATION!!";
 }
