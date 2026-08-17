f24ab90f2eabf9e26972bb5fbf6576b4267b31ab
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-17 14:34:23 +0530

    build: delete all products

--- src/ExpenseTracker/Service/TransactionService.cs
+++ src/ExpenseTracker/Service/TransactionService.cs
@@ -83,6 +83,14 @@ public class TransactionService : IService
     public void DeleteAllTransactions() => this._repository.DeleteAllTransactions();
 
     /// <summary>
+    /// Deletes all transactions in the list.
+    /// </summary>
+    public void DeleteAllTransactions()
+    {
+        this._repository.DeleteAllTransactions();
+    }
+
+    /// <summary>
     /// Update the value of transaction in transaction list.
     /// </summary>
     /// <param name="transaction"> Instance of transaction containing updated values. </param>
