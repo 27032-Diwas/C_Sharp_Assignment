b0e40b9c6aa93c6b2b03092be24e3b2cbd78ed2b
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-08-20 14:59:51 +0530

    Expense Tracker | UML Diagram and HLD File operation
    * build: UML and HLD
    
    * build: ARD

---
+++ src/ExpenseTracker/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpenseTracker;
+
+/// <summary>
+/// Entry point of the application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Starts the application and display the main menu.
+    /// </summary>
+    public static void Main()
+    {
+    }
+}
