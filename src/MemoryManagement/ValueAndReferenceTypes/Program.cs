913ea7fb935a4c9f71b70ed8cbd6004f5601ee31
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-04 12:20:50 +0530

    build: task1

---
+++ src/MemoryManagement/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ValueAndReferenceTypes;
+
+/// <summary>
+/// Entry point to the application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Starts the application and display the main menu.
+    /// </summary>
+    public static void Main()
+    {
+        int number = 10;
+        Student student = new ()
+        {
+            Name = "Diwas",
+            Age = 21,
+        };
+
+        Console.WriteLine("Before modifying data\n");
+        Console.WriteLine($"Value Type [ Integer ] = {number}");
+        Console.WriteLine($"Reference type [Student Object] age = {student.Age}");
+
+        Modify(number, student);
+
+        Console.WriteLine("\nAfter modifying data\n");
+        Console.WriteLine($"Value Type [ Integer ] = {number}");
+        Console.WriteLine($"Reference type [Student Object] age = {student.Age}");
+
+        Console.WriteLine("\nPRESS ANY KEY TO EXIT");
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Modify the data in both value and reference type.
+    /// </summary>
+    /// <param name="number"> Number - value type. </param>
+    /// <param name="student"> Instance of student - reference type. </param>
+    public static void Modify(int number, Student student)
+    {
+        number = 17;
+        student.Age = 20;
+    }
+}
---
+++ src/MemoryManagement/ValueAndReferenceTypes/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace ValueAndReferenceTypes;
+
+/// <summary>
+/// Contains properties related to students.
+/// </summary>
+public class Student
+{
+    /// <summary>
+    /// Gets or sets name.
+    /// </summary>
+    /// <value> Name of the student. </value>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets age.
+    /// </summary>
+    /// <value> Age of the student. </value>
+    public int Age { get; set; }
+}
