65f37a3c3e2aaa07bd6a2edf6ccd2d5bda277b7b
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-15 15:25:19 +0530

    fix: comments

--- src/MemoryOptimization/MemoryEater/Program.cs
+++ src/MemoryOptimization/MemoryEater/Program.cs
@@ -12,29 +12,5 @@ public class Program
     {
         MemoryEater memoryEater = new ();
         memoryEater.Allocate();
-
-        Console.ReadKey();
-    }
-}
-
-/// <summary>
-/// Contains logic to allocate memory.
-/// </summary>
-internal class MemoryEater
-{
-    private List<int[]> _memAlloc = new List<int[]>();
-
-    /// <summary>
-    /// Allocates memory and adds it to a list.
-    /// </summary>
-    public void Allocate()
-    {
-        while (true)
-        {
-            this._memAlloc.Add(new int[1000]);
-
-            // Assume memAlloc variable is used within the loop.
-            Thread.Sleep(10);
-        }
     }
 }
--- src/MemoryOptimization/MemoryEaterOptimized/Program.cs
+++ src/MemoryOptimization/MemoryEaterOptimized/Program.cs
@@ -12,29 +12,5 @@ public class Program
     {
         MemoryEater memoryEater = new ();
         memoryEater.Allocate();
-
-        Console.ReadKey();
-    }
-}
-
-/// <summary>
-/// Contains logic to allocate memory.
-/// </summary>
-internal class MemoryEater
-{
-    /// <summary>
-    /// Allocates memory and adds it to a list.
-    /// </summary>
-    public void Allocate()
-    {
-        List<int[]> memAlloc = new ();
-        int[] list = new int[1000];
-        while (true)
-        {
-            memAlloc.Add(list);
-
-            // Assume memAlloc variable is used within the loop.
-            Thread.Sleep(10);
-        }
     }
 }
