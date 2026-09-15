0b16b0cf7475c28d62ce857b11892361175ea933
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-15 17:03:16 +0530

    Memory Optimization
    * fix: tasks
    
    * fix: memory eater
    
    * fix: optimization
    
    * fix: readme
    
    * fix: learnings
    
    * fix: comments
    
    * fix: readme
    
    * fix: readme

---
+++ src/MemoryOptimization/MemoryEater/MemoryEater.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignments;
+
+/// <summary>
+/// Contains logic to allocate memory.
+/// </summary>
+internal class MemoryEater
+{
+    private List<int[]> _memAlloc = new List<int[]>();
+
+    /// <summary>
+    /// Allocates memory and adds it to a list.
+    /// </summary>
+    public void Allocate()
+    {
+        while (true)
+        {
+            this._memAlloc.Add(new int[1000]);
+
+            // Assume memAlloc variable is used within the loop.
+            Thread.Sleep(10);
+        }
+    }
+}
---
+++ src/MemoryOptimization/MemoryEater/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignments;
+
+/// <summary>
+/// Entry point to the application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Starts the program.
+    /// </summary>
+    public static void Main()
+    {
+        MemoryEater memoryEater = new ();
+        memoryEater.Allocate();
+    }
+}
