fc75f84a7d00adc9bc5a41775e3231e1900c09fc
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-18 14:08:57 +0530

    fix: delegate

---
+++ src/AdvanceTopics/Notifier.cs
@@ -0,0 +1,12 @@
+﻿namespace AdvanceTopics;
+
+/// <summary>
+/// Contains notify event and delegate.
+/// </summary>
+public class Notifier
+{
+    /// <summary>
+    /// Notify delegate.
+    /// </summary>
+    public delegate void Notify();
+}
---
+++ src/AdvanceTopics/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignments;
+
+/// <summary>
+/// Entry point to the application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Starts the application.
+    /// </summary>
+    public static void Main()
+    {
+        Console.ReadKey();
+    }
+}
