f5091f40eb048183a7de0f916683c59a80187d2c
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-18 10:32:11 +0530

    Memory Management | Value and Reference Type, Stack and Heap, GC and IDispose
    * build: task1
    
    * fix: program.cs
    
    * fix: task 4
    
    * build: task4
    
    * fix: folder structure
    
    * fix: documentation
    
    * build: readme for gc
    
    * fix: readme gc
    
    * build: readme task4
    
    * build: readme for task 1 and 2
    
    * fix: comments
    
    * fix: changes

---
+++ src/MemoryManagement/IDisposableDemo/Program.cs
@@ -0,0 +1,88 @@
+﻿using IDisposableDemo;
+
+namespace IDisposableDemo;
+
+/// <summary>
+/// Entry point to the application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Starts the program.
+    /// </summary>
+    public static void Main()
+    {
+        InitialWrite();
+
+        Console.WriteLine("File opened, used write operation and closed file using Dispose method.");
+
+        Console.WriteLine("-------------------------------------------------------\n");
+
+        WriteWithUsing();
+
+        Console.WriteLine("File opened again with using statement and used write operation.");
+
+        Console.WriteLine("-------------------------------------------------------\n");
+
+        Read();
+
+        Console.WriteLine("Read content in the file.");
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Writes first set of statement into the file.
+    /// </summary>
+    public static void InitialWrite()
+    {
+        try
+        {
+            FileOperation fileOperation = new ("File.txt");
+            fileOperation.Write("Initial write operation.");
+            fileOperation.Dispose();
+            Read();
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("File is not disposed properly.");
+        }
+    }
+
+    /// <summary>
+    /// Writes into file with using statement.
+    /// </summary>
+    public static void WriteWithUsing()
+    {
+        try
+        {
+            using (FileOperation fileOperation = new ("File.txt"))
+            {
+                fileOperation.Write("File implementation with using statement.");
+            }
+
+            Read();
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("File is not disposed properly.");
+        }
+    }
+
+    /// <summary>
+    /// Reads the content from the file.
+    /// </summary>
+    public static void Read()
+    {
+        try
+        {
+            using StreamReader streamReader = new ("File.txt");
+            string content = streamReader.ReadToEnd();
+
+            Console.WriteLine($"Content in the file : {content}");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("File is held by some other operation.");
+        }
+    }
+}
