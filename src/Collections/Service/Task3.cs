b860aaa7f3c63d0cf437ab98ba7625388327226c
27032-Diwas <diwas.thangarasu@solitontech.com>
2026-09-24 09:43:53 +0530

    Collections and Generics | List, Stack, Queue, Dictionary and IEnumerable
    * build: task1 service
    
    * build: task1
    
    * build: t2 to t6
    
    * fix: documentation
    
    * build: docs
    
    * fix: display options
    
    * fix: task6
    
    * fix: warnings
    
    * fix: constants
    
    * fix: self review
    
    * fix: task 6 improvement
    
    * fix: peer review comments
    
    * fix: warnings

---
+++ src/Collections/Service/Task3.cs
@@ -0,0 +1,37 @@
+﻿namespace Collections.Service;
+
+/// <summary>
+/// Contains operation to add, remove and view queue.
+/// </summary>
+/// <typeparam name="T"> Type of item in queue (string). </typeparam>
+public class Task3<T>
+{
+    private readonly Queue<T> _queue = new ();
+
+    /// <summary>
+    /// Adds item to the queue.
+    /// </summary>
+    /// <param name="item"> Item to add in queue. </param>
+    public void AddItem(T item) => this._queue.Enqueue(item);
+
+    /// <summary>
+    /// Removes front item in the queue.
+    /// </summary>
+    /// <returns> True if item exist in queue ;otherwise false. </returns>
+    public bool RemoveItem()
+    {
+        if (this._queue.Count == 0)
+        {
+            return false;
+        }
+
+        this._queue.Dequeue();
+        return true;
+    }
+
+    /// <summary>
+    /// Gets queue.
+    /// </summary>
+    /// <returns> Queue of items. </returns>
+    public Queue<T> GetQueue() => this._queue;
+}
